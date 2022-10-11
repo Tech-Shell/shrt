d6ba82de118a77cf27ddd623065b2c2c1f06e2e0
Vaibhav Sharma <rajmachawal19@gmail.com>
2022-10-10 20:26:02 -0700

    shortner complete

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose  = require('mongoose');
 const Links = require('./models/Links');
+const shortid = require('shortid');
 const port = process.env.PORT || 3000;
 require('dotenv').config();
 
@@ -18,6 +19,37 @@ mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology:
 app.get('/', (req, res) => {
     res.render('index');
 })
+
+let mainId = "";
+let mainUrl = "";
+
+app.get("/link", async (req,res)=>{
+    res.render('link.ejs', {shorturls : mainId, url: mainUrl})
+    mainId = "";
+    mainUrl = "";
+})
+
+app.post("/shortUrls", async(req,res)=>{
+    let id = shortid.generate()
+    mainId = id
+    mainUrl = req.body.fullurl
+    await Links.create({
+        full : mainUrl,
+        short: id,
+    });
+    res.redirect("/link")
+})
+
+app.get('/:shortUrl', async(req,res)=>{
+    const shortUrl = await Links.findOne({short: req.params.shortUrl})
+    if(shortUrl == null) return res.sendStatus(404)
+
+    shortUrl.save();
+
+    res.redirect(shortUrl.full)
+})
+
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
